5674654500733d26d74599b70abd375e9585ef17
Chris Martin <ch.martin@gmail.com>
2017-08-06 13:17:41 -0400

    code style

--- pam/src/System/Posix/PAM/Internals.hs
+++ pam/src/System/Posix/PAM/Internals.hs
@@ -54,14 +54,10 @@ instance Storable CPamMessage
    alignment = cAlignment
    sizeOf = cSizeOf
 
-{- |
-
-Used to return the user's response to the PAM library.
+{- | Used to return the user's response to the PAM library.
 
 /This structure is allocated by the application program, and it is free()'d by/
-/the Linux-PAM library (or calling module)./
-
--}
+/the Linux-PAM library (or calling module)./ -}
 data CPamResponse = CPamResponse
   { resp :: CString
   , resp_retcode :: CInt -- ^ currently un-used, zero expected
@@ -75,11 +71,7 @@ instance Storable CPamResponse
    alignment = cAlignment
    sizeOf = cSizeOf
 
-{- |
-
-The actual conversation structure itself.
-
--}
+{- | The actual conversation structure itself. -}
 data CPamConv = CPamConv
   { conv :: FunPtr ConvFunc
   , appdata_ptr :: Ptr ()
@@ -96,84 +88,56 @@ instance Storable CPamConv
    alignment = cAlignment
    sizeOf = cSizeOf
 
-{- |
+{- | An opaque handle to a PAM session, obtained using 'c_pam_start' and freed
+using 'c_pam_end'.
 
-An opaque handle to a PAM session, obtained using 'c_pam_start' and freed using
-'c_pam_end'.
-
-/You must use a different 'CPamHandle' for each transaction./
-
--}
+/You must use a different 'CPamHandle' for each transaction./ -}
 type CPamHandle = Ptr ()
 
-{- |
+{- | Creates a 'CPamHandle' and initiates a PAM transaction. This is always the
+first thing you need to do to use PAM.
 
-Creates a 'CPamHandle' and initiates a PAM transaction. This is always the first
-thing you need to do to use PAM.
-
-/Make sure you call 'pam_end' once the transaction is over./
-
--}
+/Make sure you call 'pam_end' once the transaction is over./ -}
 foreign import ccall "security/pam_appl.h pam_start" c_pam_start
   :: CString -> CString -> Ptr CPamConv -> Ptr CPamHandle -> IO CInt
 
-{- |
-
-Terminates a PAM transaction.
+{- | Terminates a PAM transaction.
 
 /The 'CPamHandle' will become invalid, so this is the last thing you should do/
-/with it./
-
--}
+/with it./ -}
 foreign import ccall "security/pam_appl.h pam_end" c_pam_end
   :: CPamHandle -> CInt -> IO CInt
 
-{- |
-
-This is used to authenticate the user. The user is required to provide an
+{- | This is used to authenticate the user. The user is required to provide an
 authentication token depending upon the authentication service. Usually this is
 a password, but could also be a fingerprint. The PAM service module may request
 that the user enter their username via the conversation mechanism (see
-'CPamConv').
-
--}
+'CPamConv'). -}
 foreign import ccall "security/pam_appl.h pam_authenticate" c_pam_authenticate
   :: CPamHandle -- ^ A PAM handle obtained by a prior call to 'c_pam_start'.
   -> PamAuthenticateFlags
   -> IO CInt
 
-{- |
-
-The binary /or/ of zero or more of the following values:
+{- | The binary /or/ of zero or more of the following values:
 
 - @PAM_SILENT@ - Do not emit any messages.
 
 - @PAM_DISALLOW_NULL_AUTHTOK@ - The PAM module service should return
-  @PAM_AUTH_ERR@ if the user does not have a registered authentication token.
-
--}
+  @PAM_AUTH_ERR@ if the user does not have a registered authentication token. -}
 type PamAuthenticateFlags = CInt
 
-{- |
-
-Used to determine if the user's account is valid. It checks for authentication
-token and account expiration and verifies access restrictions. This is typically
-called after the user has been authenticated.
-
--}
+{- | Used to determine if the user's account is valid. It checks for
+authentication token and account expiration and verifies access restrictions.
+This is typically called after the user has been authenticated. -}
 foreign import ccall "security/pam_appl.h pam_acct_mgmt" c_pam_acct_mgmt
   :: CPamHandle -- ^ A PAM handle obtained by a prior call to 'c_pam_start'.
   -> PamAcctMgmtFlags
   -> IO CInt
 
-{- |
-
-The binary /or/ of zero or more of the following values:
+{- | The binary /or/ of zero or more of the following values:
 
 - @PAM_SILENT@ - Do not emit any messages.
 
 - @PAM_DISALLOW_NULL_AUTHTOK@ - The PAM module service should return
-  @PAM_AUTH_ERR@ if the user does not have a registered authentication token.
-
--}
+  @PAM_AUTH_ERR@ if the user does not have a registered authentication token. -}
 type PamAcctMgmtFlags = CInt
--- pam/src/System/Posix/PAM/Types.hs
+++ pam/src/System/Posix/PAM/Types.hs
@@ -8,46 +8,38 @@ import Prelude (Int, String)
 import System.IO (IO)
 import Text.Show (Show)
 
-{- |
+{- | Used to pass prompting text, error messages, or other informatory text to
+the user. -}
+data PamMessage =
+  PamMessage
+    { pmString :: String
+    , pmStyle :: PamStyle
+    }
+  deriving (Show, Eq)
 
-Used to pass prompting text, error messages, or other informatory text to the
-user.
+data PamStyle =
+  PamPromptEchoOff | PamPromptEchoOn | PamErrorMsg | PamTextInfo
+  deriving (Show, Eq)
 
--}
-data PamMessage = PamMessage { pmString :: String
-                             , pmStyle :: PamStyle
-                             }
-                             deriving (Show, Eq)
-
-data PamStyle = PamPromptEchoOff
-              | PamPromptEchoOn
-              | PamErrorMsg
-              | PamTextInfo
-              deriving (Show, Eq)
-
-{- |
-
-Used to return the user's response to the PAM library.
+{- | Used to return the user's response to the PAM library.
 
 http://www.kernel.org/pub/linux/libs/pam/Linux-PAM-html/adg-interface-of-app-expected.html#adg-pam_conv
 
 The @resp_retcode@ member of the C struct is unused, so we do not bother
-including a corresponding field in this Haskell type.
-
- -}
+including a corresponding field in this Haskell type. -}
 data PamResponse = PamResponse String
-                 deriving (Show, Eq)
+  deriving (Show, Eq)
 
-data PamRetCode = PamSuccess
-                | PamRetCode Int
-                deriving (Show, Eq)
+data PamRetCode = PamSuccess | PamRetCode Int
+  deriving (Show, Eq)
 
 data PamFlag = PamFlag Int
 
 type PamConv = Ptr () -> [PamMessage] -> IO [PamResponse]
 
-
-data PamHandle = PamHandle { cPamHandle :: Ptr ()
-                           , cPamCallback :: FunPtr ConvFunc
-                           }
-                           deriving (Show, Eq)
+data PamHandle =
+  PamHandle
+    { cPamHandle :: Ptr ()
+    , cPamCallback :: FunPtr ConvFunc
+    }
+  deriving (Show, Eq)
